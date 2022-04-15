6accdf74d13fbf11fe187d3f76dc55788d2c74a6
annakraiushkina2000 <Kraiushkinih.dom@yandex.ru>
2022-04-15 11:43:56 +0300

    changed Sort.java file

--- scr/main/Sort.java
+++ scr/main/Sort.java
@@ -2,18 +2,14 @@ import java.util.Arrays;
 
 public class Sort{
     public static int[] getResult(int array []){
-    boolean isSorted = false;
-        int buf;
-        while (!isSorted) {
-            isSorted = true;
-            for (int i=0; i<mas.length-1; i++){
-                if (mas[i]> mas[i+1]){
-                    isSorted = false;
-                    buf = mas[i];
-                    mas[i] = mas[i+1];
-                    mas[i+1] = buf;
-                }
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i];
+            int j = i - 1;
+            while(j >= 0 && current < array[j]) {
+                array[j+1] = array[j];
+                j--;
             }
+            array[j+1] = current;
         }
         return array;
     }
